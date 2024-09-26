3733c8b521d9631e1d0a7c836dd9e822ac5b50c0
Tiago Villagra <tiagodomingovillagra30@gmail.com>
2024-09-26 18:10:52 -0300

    configuracion completa del codigo y agreagacion del for y array

--- js/main.js
+++ js/main.js
@@ -1,13 +1,21 @@
-const remera = 1000;
-const jean = 2000;
-const buzo = 1500;
-
+const productos = [
+    { nombre: "Remera", precio: 1000 },
+    { nombre: "Jean", precio: 2000 },
+    { nombre: "Buzo", precio: 1500 }
+];
 
 function mostrarMenu() {
-    let seleccion = prompt("¿Qué deseas comprar? (eliga la opcion correspondiente)\n1. Remera\n2. Jean\n3. Buzo");
+    let menu = "¿Qué deseas comprar? (elige la opción correspondiente)\n";
+    
+    
+    for (let i = 0; i < productos.length; i++) {
+        menu += `${i + 1}. ${productos[i].nombre} - $${productos[i].precio}\n`;
+    }
+    menu += `${productos.length + 1}. Salir`;  
+    let seleccion = prompt(menu);
+    
     return parseInt(seleccion);
 }
-
 
 function confirmarCompra(producto, precio) {
     let confirmacion = confirm(`Has seleccionado ${producto} con un precio de $${precio}. ¿Deseas confirmar tu compra?`);
@@ -18,25 +26,21 @@ function confirmarCompra(producto, precio) {
     }
 }
 
-
 function tienda() {
     let seleccion = mostrarMenu();
-    switch (seleccion) {
-        case 1:
-            confirmarCompra('Remera', remera);
-            break;
-        case 2:
-            confirmarCompra('Jean', jean);
-            break;
-        case 3:
-            confirmarCompra('Buzo', buzo);
-            break;
-        default:
-            alert("Selección no válida. Por favor, intenta de nuevo.");
-            tienda();  
+    
+    if (isNaN(seleccion) || seleccion < 1 || seleccion > productos.length + 1) {
+        alert("Selección no válida. Por favor, intenta de nuevo.");
+        tienda();  
+    } else if (seleccion === productos.length + 1) {
+        alert("Gracias por visitar nuestra tienda. ¡Hasta la próxima!");
+        return;  
+    } else {
+        let productoSeleccionado = productos[seleccion - 1];
+        confirmarCompra(productoSeleccionado.nombre, productoSeleccionado.precio);
     }
 }
 
-
 tienda();
+
 
